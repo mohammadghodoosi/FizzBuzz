8ebc7b39423f5de4a1936bb2deae26565ae4183a
Mohammad Ghodoosi <ghodusimohamad@gmail.com>
2021-08-30 15:13:03 +0430

    commit 1
    creating new option in shared file
    creating new view for it
    creating new action method with the same name in controller

--- FizzBuzz/Controllers/HomeController.cs
+++ FizzBuzz/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace FizzBuzz.Controllers
         {
             return View();
         }
+        public IActionResult FizzBuzz()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
